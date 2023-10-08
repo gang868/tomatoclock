e7501fc32fe7e8e05a692cc1b5662cfde2745301
L&J <609069481@qq.com>
2023-10-08 17:17:47 +0800

    置顶

--- tomato.py
+++ tomato.py
@@ -8,15 +8,23 @@ Email: gang868@gmail.com
 Description: Tomato alarm clock
 Version: 1.1
 '''
+from enum import Enum
 
+from PyQt5 import QtCore
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout, QHBoxLayout, QLCDNumber, QSystemTrayIcon, QMenu, QAction
 from PyQt5.QtCore import QTimer, Qt
 from PyQt5.QtGui import QPalette, QFont, QIcon
 import sys, playsound, os
+from qfluentwidgets import (CommandBar, Action)
+from qfluentwidgets import FluentIcon as FIF
+
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 OPACITY = 0.6
 
+class WorkStatus(Enum):
+    WORK = ("work", "工作")
+    REST = ("rest", "休息")
 
 class Tomato(QWidget):
     def __init__(self, app:QApplication):
@@ -28,6 +36,7 @@ class Tomato(QWidget):
         self.rest = 5  # 休息时间5分钟
         self.round_rest = 30  # 1轮4个番茄钟休息30分钟
         self.current_status = "Work"
+        self.is_top_hint = False
         self.initUI()
 
     def initUI(self):
@@ -42,7 +51,7 @@ class Tomato(QWidget):
         self.setGeometry(x, y, w, h)
         # self.setWindowOpacity(OPACITY)
         # 窗体置顶
-        self.setWindowFlags(Qt.WindowStaysOnTopHint)
+        # self.setWindowFlags(Qt.WindowStaysOnTopHint)
 
         # 设置番茄图标（程序和托盘）
         self.icon = QIcon(os.path.join(BASE_DIR, 'tomato.svg'))
@@ -65,6 +74,10 @@ class Tomato(QWidget):
         self.timer.timeout.connect(self.onTimer)  # 绑定定时触发事件
 
         vbox = QVBoxLayout()
+
+        # bar = self.createCommandBar()
+        # vbox.addWidget(bar, 0, Qt.AlignTop)
+
         # 提示标签
         self.labelRound = QLabel(self)  # 提示标签
         self.labelRound.setText("准备开始番茄钟")
@@ -90,24 +103,94 @@ class Tomato(QWidget):
         hbox = QHBoxLayout()
         vbox.addLayout(hbox)
         # 功能按钮
-        self.startButton = QPushButton("开始")
-        self.startButton.clicked.connect(self.start)
-        hbox.addWidget(self.startButton)
+        # self.startButton = QPushButton("开始")
+        # self.startButton.clicked.connect(self.start)
+        # hbox.addWidget(self.startButton)
+        self.startPauseButton = QPushButton()
+        self.startPauseButton.setText("开始")
+        self.startPauseButton.clicked.connect(self.toggleStartPause)
+        hbox.addWidget(self.startPauseButton)
 
         self.stopButton = QPushButton("停止")
         self.stopButton.setEnabled(False)
         self.stopButton.clicked.connect(self.stop)
         hbox.addWidget(self.stopButton)
 
-        self.pauseButton = QPushButton("暂停")
-        self.pauseButton.setEnabled(False)
-        self.pauseButton.clicked.connect(self.pause)
-        hbox.addWidget(self.pauseButton)
+        # self.pauseButton = QPushButton("暂停")
+        # self.pauseButton.setEnabled(False)
+        # self.pauseButton.clicked.connect(self.pause)
+        # hbox.addWidget(self.pauseButton)
+
+        self.topHintButton = QPushButton("置顶")
+        self.topHintButton.clicked.connect(self.toggleTopHint)
+        hbox.addWidget(self.topHintButton)
 
         self.setLayout(vbox)
 
         self.tray.show()
         self.show()
+
+    def createCommandBar(self):
+        self.commandBar = bar = CommandBar(self)
+        bar.setToolButtonStyle(Qt.ToolButtonTextBesideIcon)
+        addAction = Action(FIF.ADD, self.tr('Add'))
+        addAction.triggered.connect(lambda: print("Adddddddddddddddd"))
+        self.closeAction = closeAction = Action(FIF.CLOSE, self.tr('Close'))
+        closeAction.triggered.connect(self.close)
+        self.minAction = minAction = Action(FIF.MINIMIZE, self.tr('Minimize'))
+        # minAction.setIcon()
+        minAction.triggered.connect(self.showMinimized)
+        self.maxAction = maxAction = Action(FIF.FULL_SCREEN, self.tr('FullScreen'))
+        maxAction.triggered.connect(self.switchMaxNormal)
+        maxAction.setObjectName("maxAction")
+        self.normalAction = normalAction = Action(FIF.BACK_TO_WINDOW, self.tr('BackToWindow'))
+        normalAction.triggered.connect(self.switchMaxNormal)
+        normalAction.setObjectName("normalAction")
+        bar.setWindowTitle("xxxxxxxxxxxx")
+        bar.addActions([
+            minAction,
+            # normalAction,
+            maxAction,
+            closeAction,
+            # Action(FIF.ROTATE, self.tr('Rotate')),
+            # Action(FIF.ZOOM_IN, self.tr('Zoom in')),
+            # Action(FIF.ZOOM_OUT, self.tr('Zoom out')),
+        ])
+        # bar.addSeparator()
+
+        # add custom widget
+        # button = TransparentDropDownPushButton(self.tr('Sort'), self, FIF.SCROLL)
+        # button.setMenu(self.createCheckableMenu())
+        # button.setFixedHeight(34)
+        # setFont(button, 12)
+        # bar.addWidget(button)
+
+        # bar.addHiddenActions([
+        #     Action(FIF.SETTING, self.tr('Settings'), shortcut='Ctrl+I'),
+        # ])
+        return bar
+
+    # 判断最大化，还是窗口化
+    def switchMaxNormal(self):
+        try:
+            # maxAction = [item for item in self.commandBar.actions() if item.objectName() == 'maxAction'][0]
+            # normalAction = [item for item in self.commandBar.actions() if item.objectName() == 'normalAction'][0]
+            if self.isMaximized():
+                self.showNormal()                  #窗口化显示
+                # maxAction.setVisible(False)
+                # normalAction.setVisible(True)
+                print("取消最大化")
+                self.commandBar.insertAction(self.closeAction, self.maxAction)
+                self.commandBar.removeAction(self.normalAction)
+            else:
+                self.showMaximized()               #最大化显示
+                # maxAction.setVisible(True)
+                # normalAction.setVisible(False)
+                print("最大化")
+                self.commandBar.insertAction(self.closeAction, self.normalAction)
+                self.commandBar.removeAction(self.maxAction)
+        except Exception as e:
+            print("switchMaxNormal Exception", e)
 
     def closeEvent(self, event):
         # 禁止关闭按钮退出程序
@@ -165,10 +248,11 @@ class Tomato(QWidget):
         self.second_remain = self.work * 60
         self.current_status = 'Work'
         self.clock.display("%2d:%02d" % (self.work, 0))
-        self.startButton.setEnabled(True)
-        self.pauseButton.setEnabled(False)
+        # self.startButton.setEnabled(True)
+        # self.pauseButton.setEnabled(False)
         self.stopButton.setEnabled(False)
         self.timer.stop()
+        self.startPauseButton.setText('开始')
 
     def pause(self):
         self.startButton.setEnabled(True)
@@ -176,11 +260,51 @@ class Tomato(QWidget):
         self.stopButton.setEnabled(True)
         self.timer.stop()
 
-    # def enterEvent(self, event: QtCore.QEvent) -> None:
-    #     # self.setWindowOpacity(1)
-    #     print("enterEvent", event)
-    # def leaveEvent(self, event: QtCore.QEvent) -> None:
-    #     self.setWindowOpacity(OPACITY)
+    def toggleStartPause(self):
+        t = self.startPauseButton.text()
+        if t == '开始' or t == '继续':
+            # 启动定时器
+            self.timer.start()
+            # 设置功能按钮
+            self.stopButton.setEnabled(True)
+            self.startPauseButton.setText('暂停')
+        elif t == '暂停':
+            self.stopButton.setEnabled(True)
+            self.timer.stop()
+            self.startPauseButton.setText('继续')
+
+    def toggleTopHint(self):
+        t  = self.topHintButton.text()
+        self.is_top_hint = True if not self.is_top_hint else False # 取反
+        if self.is_top_hint:
+            self.setWindowFlag(Qt.WindowStaysOnTopHint, True)
+            self.topHintButton.setText('取消置顶')
+            self.toggleHideSome()
+        else:
+            self.setWindowFlag(Qt.WindowStaysOnTopHint, False)
+            self.topHintButton.setText('置顶')
+            self.toggleHideSome()
+        # self.show() # fix 窗体消失问题
+        self.setVisible(True)
+
+    def toggleHideSome(self, is_hide=None):
+        is_hide = is_hide if is_hide is not None else self.is_top_hint
+        if is_hide:
+            self.startPauseButton.setVisible(False)
+            self.stopButton.setVisible(False)
+        else:
+            self.startPauseButton.setVisible(True)
+            self.stopButton.setVisible(True)
+
+    def enterEvent(self, event: QtCore.QEvent) -> None:
+        # self.setWindowOpacity(1)
+        # print("enterEvent", event)
+        self.toggleHideSome(False)
+
+    def leaveEvent(self, event: QtCore.QEvent) -> None:
+        # self.setWindowOpacity(OPACITY)
+        self.toggleHideSome()
+
     # 添加 复写 窗口状态改变 函数
     # def changeEvent(self, event):
     #     print(event)
